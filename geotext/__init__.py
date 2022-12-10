5bd0705de1f8c65994342f45291f55d1136d80d5
kdm004 <97989597+kdm004@users.noreply.github.com>
2022-12-09 21:17:41 -0500

    Update __init__.py

--- geotext/__init__.py
+++ geotext/__init__.py
@@ -4,4 +4,4 @@ __author__ = 'Yaser Martinez Palenzuela'
 __email__ = 'yaser.martinez@gmail.com'
 __version__ = '0.2.0'
 
-#from .geotext import GeoText      trying to solve circular dependency
+from .geotext import GeoText      # commenting this out to try to solve circular dependency didn't work
